5456ab4f92aa7afdd5b05cf135cc9e7aa40efc7e
Alex.Krupnik <krupnik_a@ukr.net>
2020-07-31 00:01:46 +0300

    page 279

--- main.go
+++ main.go
@@ -8,13 +8,32 @@ type subscriber struct {
 	active bool
 }
 
+func printInfo(s subscriber) {
+	fmt.Println("Name:", s.name)
+	fmt.Println("Monthly rate:", s.rate)
+	fmt.Println("Active?:", s.active)
+}
+
+func defaultSubscriber(name string) subscriber {
+	var s subscriber
+	s.name = name
+	s.rate = 5.99
+	s.active = true
+	return s
+}
+
+func applyDiscount(s *subscriber) {
+	s.rate = 4.99
+}
+
 func main() {
 
-	var subscriber1 subscriber
-	subscriber1.name = "Aman Sing"
-	fmt.Println("Name:", subscriber1.name)
-	var subscriber2 subscriber
-	subscriber2.name = "Betn Ryan"
+	subscriber1 := defaultSubscriber("Aman Sing")
+	applyDiscount(&subscriber1)
+	printInfo(subscriber1)
+
+	subscriber2 := defaultSubscriber("Betn Ryan")
 	fmt.Println("Name:", subscriber2.name)
+	printInfo(subscriber2)
 
 }
