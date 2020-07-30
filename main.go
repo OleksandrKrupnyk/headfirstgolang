92396bcf41ee52b72b04985aa80397391661c9ac
Alex.Krupnik <krupnik_a@ukr.net>
2020-07-31 00:38:57 +0300

    page 302

--- main.go
+++ main.go
@@ -2,38 +2,13 @@ package main
 
 import (
 	"fmt"
-	"magazine"
 )
 
-func printInfo(s *magazine.Subscriber) {
-	fmt.Println("Name:", s.Name)
-	fmt.Println("Monthly rate:", s.Rate)
-	fmt.Println("Active?:", s.Active)
-}
-
-func defaultSubscriber(name string) *magazine.Subscriber {
-	var s magazine.Subscriber
-	s.Name = name
-	s.Rate = 5.99
-	s.Active = true
-	return &s
-}
-
-func applyDiscount(s *magazine.Subscriber) {
-	s.Rate = 4.99
-}
+type Liters float64
+type Gallons float64
 
 func main() {
-
-	subscriber1 := defaultSubscriber("Aman Sing")
-	applyDiscount(subscriber1)
-	printInfo(subscriber1)
-
-	subscriber2 := defaultSubscriber("Betn Ryan")
-	printInfo(subscriber2)
-
-	employee := magazine.Employee{Name: "Jorg", Salary: 500.0}
-	address := magazine.Address{Street: "North Ave", City: "New York", State: "NY", PostalCode: "644888"}
-	employee.Address = address
-	fmt.Println(employee.City)
+	carFuel := Gallons(Liters(240) * 0.264)
+	busFuel := Liters(Gallons(10.0) * 3.785)
+	fmt.Printf("Gallons %0.1f Liters %0.1f", carFuel, busFuel)
 }
