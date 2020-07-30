956999de5f2a39d8ebb6a960c0e8e989e3499bd3
Alex.Krupnik <krupnik_a@ukr.net>
2020-07-31 00:45:10 +0300

    page 305

--- main.go
+++ main.go
@@ -7,8 +7,21 @@ import (
 type Liters float64
 type Gallons float64
 
+// To Gallons
+func ToGallons(l Liters) Gallons {
+	return Gallons(l * 0.264)
+}
+
+// To Liters
+func ToLiters(g Gallons) Liters {
+	return Liters(g * 3.785)
+}
+
 func main() {
-	carFuel := Gallons(Liters(240) * 0.264)
-	busFuel := Liters(Gallons(10.0) * 3.785)
-	fmt.Printf("Gallons %0.1f Liters %0.1f", carFuel, busFuel)
+	carFuel := Gallons(1.2)
+	busFuel := Liters(4.5)
+	carFuel += ToGallons(Liters(40.0))
+	busFuel += ToLiters(Gallons(30.0))
+	fmt.Printf("Car fuel %0.1f gallons\n", carFuel)
+	fmt.Printf("Bus fuel %0.1f liters\n", busFuel)
 }
