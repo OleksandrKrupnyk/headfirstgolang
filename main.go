4222451a1de2591624c1fa70fbf6f6c822daafd0
Alex.Krupnik <krupnik_a@ukr.net>
2020-08-02 00:25:14 +0300

    page 358

--- main.go
+++ main.go
@@ -1,29 +1,16 @@
 package main
 
-import (
-	"calendar"
-	"fmt"
-	"log"
-)
+import "gadget"
+
+func playList(device gadget.TapePlayer, songs []string) {
+	for _, song := range songs {
+		device.Play(song)
+	}
+	device.Stop()
+}
 
 func main() {
-	event := calendar.Event{}
-	err := event.SetDay(3)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = event.SetMonth(8)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = event.SetYear(2020)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = event.SetTitle("Hello me!")
-	fmt.Println(event.Year())
-	fmt.Println(event.Month())
-	fmt.Println(event.Day())
-	fmt.Println(event.Title())
+	player := gadget.TapePlayer{}
+	mixtape := []string{"Jessie's Girl", "Whip It", "9 to 5"}
+	playList(player, mixtape)
 }
