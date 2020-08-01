9b5604ca5f775a45b1cf3ab9a827a8e81278ba7f
Alex.Krupnik <krupnik_a@ukr.net>
2020-08-01 23:30:30 +0300

    page 331

---
+++ date.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+)
+
+type Date struct {
+	Year  int
+	Month int
+	Day   int
+}
+
+func (d *Date) SetYear(year int) error {
+	if year < 1 {
+		return errors.New("wrong year")
+	}
+	d.Year = year
+	return nil
+}
+func (d *Date) SetMonth(month int) error {
+	if month < 1 || month > 12 {
+		return errors.New("wrong month")
+	}
+	d.Month = month
+	return nil
+}
+func (d *Date) SetDay(day int) error {
+	if day < 1 || day > 31 {
+		return errors.New("wrong day")
+	}
+	d.Day = day
+	return nil
+}
+
+func main() {
+	date := Date{}
+	err := date.SetYear(0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetMonth(45)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(89)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(date)
+}
