65a6eeef58d40539806fc9b656ea43247448a636
Alex.Krupnik <krupnik_a@ukr.net>
2020-08-05 22:13:15 +0300

    page 401

--- main.go
+++ main.go
@@ -3,35 +3,25 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"log"
 	"path/filepath"
 )
 
-func scanDirectory(path string) error {
+func scanDirectory(path string) {
 	fmt.Println(path)
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		fmt.Printf("Returning error from scanDirrectory(\"%s\") call\n", path)
-		return err
+		panic(err)
 	}
 	for _, file := range files {
 		filePath := filepath.Join(path, file.Name())
 		if file.IsDir() {
-			err := scanDirectory(filePath)
-			if err != nil {
-				fmt.Printf("Returning error from scanDirrectory(\"%s\") call\n", path)
-				return err
-			}
+			scanDirectory(filePath)
 		} else {
 			fmt.Println("File:", file.Name())
 		}
 	}
-	return nil
 }
 
 func main() {
-	err := scanDirectory("c:\\users")
-	if err != nil {
-		log.Fatal(err)
-	}
+	scanDirectory("c:\\users")
 }
