d24792548ba36c0b0c812b975f29de8fbd44745a
Alex.Krupnik <krupnik_a@ukr.net>
2020-08-05 22:10:11 +0300

    page 398

--- main.go
+++ main.go
@@ -4,19 +4,34 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"path/filepath"
 )
 
-func main() {
-	files, err := ioutil.ReadDir("d:\\")
+func scanDirectory(path string) error {
+	fmt.Println(path)
+	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Returning error from scanDirrectory(\"%s\") call\n", path)
+		return err
 	}
-
 	for _, file := range files {
+		filePath := filepath.Join(path, file.Name())
 		if file.IsDir() {
-			fmt.Println("Directory:", file.Name())
+			err := scanDirectory(filePath)
+			if err != nil {
+				fmt.Printf("Returning error from scanDirrectory(\"%s\") call\n", path)
+				return err
+			}
 		} else {
 			fmt.Println("File:", file.Name())
 		}
+	}
+	return nil
+}
+
+func main() {
+	err := scanDirectory("c:\\users")
+	if err != nil {
+		log.Fatal(err)
 	}
 }
