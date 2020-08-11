9daaf8f40878561469b6bbb45210263640fb5a2d
Alex.Krupnik <krupnik_a@ukr.net>
2020-08-11 23:43:24 +0300

    page 444

---
+++ src/prose/join_test.go
@@ -0,0 +1,21 @@
+package prose
+
+import "testing"
+
+func TestTwoElements(t *testing.T) {
+	list := []string{"apple", "orange"}
+	want := "apple and orange"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+	}
+}
+
+func TestThreeElements(t *testing.T) {
+	list := []string{"apple", "orange", "pear"}
+	want := "apple, orange, and pear"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+	}
+}
