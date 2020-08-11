ff1c89af154afe31f6574fcf1cdd6ff4bea97749
Alex.Krupnik <krupnik_a@ukr.net>
2020-08-11 23:47:42 +0300

    page 448

--- src/prose/join_test.go
+++ src/prose/join_test.go
@@ -1,13 +1,25 @@
 package prose
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func TestOneElement(t *testing.T) {
+	list := []string{"apple"}
+	want := "apple"
+	got := JoinWithCommas(list)
+	if got != want {
+		t.Error(errorString(list, got, want))
+	}
+}
 
 func TestTwoElements(t *testing.T) {
 	list := []string{"apple", "orange"}
 	want := "apple and orange"
 	got := JoinWithCommas(list)
 	if got != want {
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+		t.Error(errorString(list, got, want))
 	}
 }
 
@@ -16,6 +28,9 @@ func TestThreeElements(t *testing.T) {
 	want := "apple, orange, and pear"
 	got := JoinWithCommas(list)
 	if got != want {
-		t.Errorf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
+		t.Error(errorString(list, got, want))
 	}
+}
+func errorString(list []string, got string, want string) string {
+	return fmt.Sprintf("JoinWithCommas(%#v) = \"%s\", want \"%s\"", list, got, want)
 }
